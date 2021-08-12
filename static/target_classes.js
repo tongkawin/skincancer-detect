7768ad9232bf6aee1d429901a3e581f2eea21799
Kawin Phonklam <kawin.ph@mail.wu.ac.th>
2021-08-12 16:28:31 +0700

    version 1

---
+++ static/target_classes.js
@@ -0,0 +1,18 @@
+TARGET_CLASSES = {
+  0: "Malignant Melanoma (mel)",
+  1: "Melanocytic Nevi (nv)",
+  2: "Seborrheic Keratosis (bkl)",
+};
+
+
+/*
+TARGET_CLASSES = {
+  0: "Actinic Keratoses and intraepithelial Carcinoma (akiec)",
+  1: "Basal Cell Carcinoma (bcc)",
+  2: "Seborrheic Keratosis (bkl)",
+  3: "Dermatofibroma (df)",
+  4: "Malignant Melanoma (mel)",
+  5: "Melanocytic Nevi (nv)",
+  6: "Vascular Lesions (vasc)"
+};
+*/
